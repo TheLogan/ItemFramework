ad96901d7441f899ac0bac26dbd57ddb1cea57ea
Tim Logan <timlogan475@gmail.com>
2015-11-30 16:17:58 +0100

    Added width back to shaped crafting recipe

--- Assets/ItemFramework/ShapedCraftingRecipe.cs
+++ Assets/ItemFramework/ShapedCraftingRecipe.cs
@@ -6,8 +6,12 @@ namespace ItemFramework
 {
 	public class ShapedCraftingRecipe : CraftingRecipe
 	{
+		public int width;
+		
 		public override bool CheckRecipe(Container input)
 		{
+			
+
 			var inputStacks = input.ItemStacks;
 			return !Ingredients.Where((t, i) => t.Item.GetType() != inputStacks[i].GetType() ||
 			t.Amount > inputStacks[i].Amount).Any(); 
